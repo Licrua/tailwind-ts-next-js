531482a00d887f93fb6fc94ac8ef3e078eee86ff
Илья <130059272+Licrua@users.noreply.github.com>
2025-03-14 14:58:20 +0300

    Update AboutSection.tsx

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -2,7 +2,7 @@ export default function AboutSection () {
 	return (
 		 <section className="flex max-sm:p-20 max-sm:justify-center  flex-col sm:p-10 xl:p-0 mt-[121px] max-w-[616px] min-h-[600px]  text-white ">
 		 <p className="text-base  mb-5">A place where</p>
-		 <h1 className="xl:text-6xl max-sm:text-2xl sm:text-4xl font-bold leading-[74px]">A creative agency for redemptive brands</h1>
+		 <h1 className="xl:text-6xl max-sm:text-2xl sm:text-4xl font-bold leading-[80px]">A creative agency for redemptive brands</h1>
 		 <p  className="text-base mt-6">Anteelo is a leading strategic design firm that builds powerful digital solutions for startups and enterprises.</p>
 		 <a className="text-[#FFD723] mt-6 hover:underline hover:scale-110 text-sm" href="#">
 			<span className="inline-block w-5">
